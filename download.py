4faa35d515b05034a753673b09cb19896011449e
Michael Koch <mitsch@mitschlappy>
2016-03-13 10:16:18 +0100

    more general exception handling for retrieving url content filename finding handles empty basename

--- download.py
+++ download.py
@@ -5,27 +5,35 @@ import posixpath
 import urlparse
 import getopt
 import urllib
+#import urllib.error
+import os
 
-def get_file_name(fileURL):
-	path = urlparse.urlsplit(fileURL).path
-	return posixpath.basename(path)
+def get_file_name_from_url(fileURL):
+	urlSplitting = urlparse.urlsplit(fileURL)
+	basename = posixpath.basename(urlSplitting.path)
+	hostname = urlSplitting.hostname
+	if basename:
+		return basename
+	elif hostname:
+		return hostname + "_index.html"
+	else:
+		return "index.html"
 
 def download_to_disk(sourceURL, targetDirectory, targetFileName=None):
-	if targetFileName == None:
-		targetFileName = get_file_name(sourceURL)
+	targetName = get_file_name_from_url(sourceURL)
+	targetPath = os.path.join(targetDirectory, targetName)
 	try:
-		targetFile = urllib.URLopener()
-		targetFile.retrieve(sourceURL, targetDirectory + targetFileName)
-		print(sourceURL + " -> " + targetFileName + ": ok")
-	except IOError as err:
-		print(sourceURL + " -> " + targetFileName + ": " + err.strerror)
+		urllib.urlretrieve(sourceURL, targetPath)
+		print(sourceURL + " -> " + targetPath)
+	except EnvironmentError as err:
+		print("error: " + sourceURL + " -> " + targetPath + ": " + err.strerror)
 
 
 def download_from_file_to_disk(fileName, targetDirectory):
 	with open(fileName) as f:
 		for line in f:
 			download_to_disk(line.rstrip(), targetDirectory)
-
+	# TODO catch exception for not existing fileName or not existing targetDirectory
 
 
 def usage():
